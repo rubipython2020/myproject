92d9ef4e531436d2921271249f95c0feb55d49f2
RubiSingh1998 <errubisingh1998@gmail.com>
2020-12-09 20:12:02 +0530

    firstcommit

---
+++ django12/app1/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from app1.models import courseform, login ,feedback,register
+
+admin.site.register(courseform)
+admin.site.register(login)
+admin.site.register(feedback)
+admin.site.register(register)
+
